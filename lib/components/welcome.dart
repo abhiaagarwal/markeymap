0b39f8051aedf576552f2ecf757a6090cf46e613
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-06 10:21:22 -0400

    Add Welcome Video

--- lib/components/welcome.dart
+++ lib/components/welcome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:video_player/video_player.dart';
+
 import 'package:markeymap/theme.dart';
 import 'package:markeymap/popup.dart';
 
@@ -17,26 +19,37 @@ class WelcomeButton extends StatelessWidget {
       );
 }
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-          child: Column(
-            children: <Widget>[
-              _WelcomeHeader(),
-            ],
-          ),
-        ),
-      );
+  _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeHeader extends StatelessWidget {
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  VideoPlayerController _controller;
+
   @override
-  Widget build(BuildContext context) => Text(
-        'Welcome to the Markey Map!'.toUpperCase(),
-        style: MarkeyMapTheme.welcomeHeaderStyle,
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/intro_video.mp4')
+      ..initialize()
+      ..setLooping(true)
+      ..play();
+  }
+
+  BoxDecoration get _decoration => const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/texture.png'),
+          repeat: ImageRepeat.repeat,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: _decoration,
+        child: Center(
+          child: VideoPlayer(_controller),
+        ),
       );
 }
