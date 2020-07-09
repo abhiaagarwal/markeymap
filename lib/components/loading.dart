3702fda46d479ab475dc2eeee26da2688145db67
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-08 23:58:36 -0400

    Update loading

--- lib/components/loading.dart
+++ lib/components/loading.dart
@@ -8,12 +8,6 @@ import 'package:markeymap/theme.dart';
 class Loading extends StatefulWidget {
   const Loading({Key key}) : super(key: key);
 
-  static const List<String> funFacts = <String>[
-    'Ed is hot and sexy',
-    'Ed does cool things too',
-    'hiii ed markey hiiii ',
-  ];
-
   @override
   _LoadingState createState() => _LoadingState();
 }
@@ -22,13 +16,19 @@ class _LoadingState extends State<Loading> {
   Timer _timer;
   int _index = 0;
 
+  static const List<String> funFacts = <String>[
+    'Ed is hot and sexy',
+    'Ed does cool things too',
+    'hiii ed markey hiiii',
+  ];
+
   @override
   void initState() {
     super.initState();
     _timer = Timer.periodic(
       const Duration(seconds: 1),
       (Timer timer) => setState(
-        () => _index = Random(timer.tick).nextInt(Loading.funFacts.length),
+        () => _index = Random(timer.tick).nextInt(funFacts.length),
       ),
     );
   }
@@ -48,9 +48,9 @@ class _LoadingState extends State<Loading> {
             children: <Widget>[
               const CircularProgressIndicator(),
               Text(
-                'Did you know: ${Loading.funFacts[_index]}',
+                'Did you know: ${funFacts[_index]}',
                 style:
-                    MarkeyMapTheme.searchBarStyle.copyWith(color: Colors.black),
+                    MarkeyMapTheme.funFactStyle,
               ),
             ],
           ),
