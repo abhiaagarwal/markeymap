41235754678e68334c4fa267c2d46cd0871c5743
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-17 16:45:53 -0400

    Link youtube video

--- lib/components/welcome.dart
+++ lib/components/welcome.dart
@@ -1,10 +1,13 @@
+import 'dart:html';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 import 'package:markeymap/theme.dart';
 import 'package:markeymap/resources.dart' as resources;
 
-import 'package:video_player/video_player.dart';
+// import 'package:video_player/video_player.dart';
 
+/*
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key key}) : super(key: key);
 
@@ -49,4 +52,37 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           child: VideoPlayer(_controller),
         ),
       ));
+}
+*/
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key key}) : super(key: key);
+
+  BoxDecoration get _decoration => const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(resources.Image.texture),
+          repeat: ImageRepeat.repeat,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    final IFrameElement _iframeElement = IFrameElement()
+      ..width = '560'
+      ..height = '315'
+      ..src = 'https://www.youtube.com/embed/mzBePgUCV4I'
+      ..style.border = 'none';
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'iframeElement',
+      (int viewId) => _iframeElement,
+    );
+    return Container(
+      decoration: _decoration,
+      child: const HtmlElementView(
+        viewType: 'iframeElement',
+      ),
+    );
+  }
 }
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,7 +9,6 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
-import 'package:video_player_web/video_player_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -19,6 +18,5 @@ void registerPlugins(PluginRegistry registry) {
   FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
-  VideoPlayerPlugin.registerWith(registry.registrarFor(VideoPlayerPlugin));
   registry.registerMessageHandler();
 }
