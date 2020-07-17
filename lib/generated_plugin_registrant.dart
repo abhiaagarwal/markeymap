fad436eb12cb9d21a316441155470f1e8b8131a4
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-17 13:33:00 -0400

    Add manual firebase imports

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,6 +5,7 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -14,6 +15,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FirebaseFirestoreWeb.registerWith(registry.registrarFor(FirebaseFirestoreWeb));
   FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
