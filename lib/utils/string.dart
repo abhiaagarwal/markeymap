18be0c37eb0a86028ec02a0642b0cab2acd08133
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-17 20:48:20 -0400

    Catch all exceptions

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -1,9 +1,10 @@
 extension CapitalizeExtension on String {
-  String toCapitalize() { 
-    try {return split(' ')
-      .map((String part) => part[0].toUpperCase() + part.substring(1))
-      .join(' ');
-    } on RangeError {
+  String toCapitalize() {
+    try {
+      return split(' ')
+          .map((String part) => part[0].toUpperCase() + part.substring(1))
+          .join(' ');
+    } on Exception {
       return this[0].toUpperCase() + substring(1);
     }
   }
