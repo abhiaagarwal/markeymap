b4b475433eab75321e33acab8d50184fdedded49
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-17 20:30:57 -0400

    Fix Capitalize

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -1,5 +1,10 @@
 extension CapitalizeExtension on String {
-  String toCapitalize() => split(' ')
+  String toCapitalize() { 
+    try {return split(' ')
       .map((String part) => part[0].toUpperCase() + part.substring(1))
       .join(' ');
+    } on RangeError {
+      return this[0].toUpperCase() + substring(1);
+    }
+  }
 }
