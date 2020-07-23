b4913fab09a86007869a167fb1207414f806c93f
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-23 13:39:58 -0400

    Add EdActions back in
    Temporary measure

--- lib/models/town.dart
+++ lib/models/town.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/foundation.dart';
+import 'package:markeymap/models/action.dart';
 
 class Town with DiagnosticableTreeMixin {
   final String name;
   final String zipcode;
-  const Town({@required this.name, @required this.zipcode})
+  final List<EdAction> actions;
+  const Town(
+      {@required this.name, @required this.actions, @required this.zipcode})
       : assert(name != null),
         assert(zipcode != null);
 
   Town.fromMap(Map<String, dynamic> data)
       : name = data['name'] as String,
+        actions = (data['actions'] as List<Map<String, dynamic>>)
+            .map<EdAction>(
+                (Map<String, dynamic> action) => EdAction.fromMap(action))
+            .toList(),
         zipcode = data['zipcode'] as String;
 
   Map<String, dynamic> toMap() => <String, dynamic>{
         'name': name,
+        'actions': actions
+            .map<Map<String, dynamic>>((EdAction action) => action.toMap())
+            .toList(),
         'zipcode': zipcode,
       };
 
@@ -21,6 +31,7 @@ class Town with DiagnosticableTreeMixin {
     super.debugFillProperties(properties);
     properties
       ..add(StringProperty('name', name))
+      ..add(IterableProperty<EdAction>('actions', actions))
       ..add(StringProperty('zipcode', zipcode));
   }
 }
