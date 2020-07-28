8ec43da1bb92755940c40cd60bd7715fc2148f5f
Abhi Agarwal <goldenercobra@gmail.com>
2020-07-27 22:12:02 -0400

    Removed deferred loading
    Causes cache issues I think

--- lib/translations/messages_es.dart
+++ lib/translations/messages_es.dart
@@ -19,6 +19,25 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'es';
 
+  String lookupMessage(
+      String message_str,
+      String locale,
+      String name,
+      List<dynamic> args,
+      String meaning,
+      {MessageIfAbsent ifAbsent}) {
+    String failedLookup(String message_str, List<dynamic> args) {
+      // If there's no message_str, then we are an internal lookup, e.g. an
+      // embedded plural, and shouldn't fail.
+      if (message_str == null) return null;
+      throw new UnsupportedError(
+          "No translation found for message '$name',\n"
+          "  original text '$message_str'");
+    }
+    return super.lookupMessage(message_str, locale, name, args, meaning,
+        ifAbsent: ifAbsent ?? failedLookup);
+  }
+
   static m0(county) => "Condado de ${county}";
 
   static m1(amount) => "Total de fondos asegurados: ${amount}";
